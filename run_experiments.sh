234a0b0828385a00752346512a5bf2e757c07e72
LJ Smalbil <louksmalbil@gmail.com>
2025-08-04 13:46:41 +0200

    Update run_experiments.sh

--- run_experiments.sh
+++ run_experiments.sh
@@ -4,11 +4,4 @@
 # SET SEED
 # python process_news.py
 
-#python main_cs_pz.py --beta=0 --gamma=0 --setting="mimic" --num_epochs=600
-#
-# python main_hsic.py --setting="mimic" --num_epochs=600 
-# python main_giks.py --setting="mimic" --num_epochs=600 
-# python main_wass.py --setting="mimic" --num_epochs=600 
-python main_cs_pz.py --setting="mimic" --num_epochs=600 --gamma=0.001 --beta=0.1 # <- best performing
-python main_cs_pz.py --setting="mimic" --num_epochs=600 --gamma=0.00 --beta=0.1 # <- best performing
-python main_cs_pz.py --setting="mimic" --num_epochs=600 --gamma=0.001 --beta=0 # <- best performing
+python main_cs_pz.py --setting="news" --num_epochs=600 --gamma=0.001 --beta=0.1 # <- best performing
